494fea1eac7c268d653b325c3d0ee30d34d8ec4e
nekanat <nekanat.stock@gmail.com>
2018-09-14 21:22:16 +0900

    calc shape

--- a3c.py
+++ a3c.py
@@ -12,13 +12,15 @@ from libs import replay_memory, utils, optimizers, wrapped_env
 vis = visdom.Visdom()
 
 class ActorCritic(nn.Module):
-    def __init__(self, n_action):
+    def __init__(self, n_action, input_shape=(84, 84, 4)):
         super(ActorCritic, self).__init__()
         self.n_action = n_action
         self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        self.fc= nn.Linear(3136, 512)
+        r = int((int(input_shape[0] / 4) - 1) / 2) - 3
+        c = int((int(input_shape[1] / 4) - 1) / 2) - 3
+        self.fc= nn.Linear(r * c * 64, 512)
         self.pi = nn.Linear(512, self.n_action)
         self.v = nn.Linear(512, 1)
 
