fbc2782f50ff985fb67c37bc175db8cff0136514
nekanat <nekanat.stock@gmail.com>
2018-09-14 21:18:25 +0900

    use cartpole in cem

--- cem.py
+++ cem.py
@@ -15,19 +15,13 @@ class Actor(nn.Module):
     def __init__(self, n_action):
         super(Actor, self).__init__()
         self.n_action = n_action
-        self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        self.fc1 = nn.Linear(3136, 512)
-        self.fc2 = nn.Linear(512, self.n_action)
+        self.fc1 = nn.Linear(4, 32)
+        self.fc2 = nn.Linear(32, self.n_action)
         self.n_params = [torch.numel(w) for w in self.parameters()]
         self.n_param = sum(self.n_params)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.fc1(x.view(x.size(0), -1)))
+        x = F.relu(self.fc1(x))
         return self.fc2(x)
 
     def set_weight(self, mean, std):
@@ -50,21 +44,20 @@ class Actor(nn.Module):
 
 BATCH_SIZE = 64
 
-env = gym.make('MultiFrameBreakout-v0')
+env = gym.make('CartPole-v0')
 actor = Actor(env.action_space.n).to(device)
-win1 = vis.image(utils.preprocess(env.env._get_image()))
-win2 = vis.line(X=np.array([0]), Y=np.array([0.0]),
-                opts=dict(title='Score'))
+win = vis.line(X=np.array([0]), Y=np.array([0.0]),
+               opts=dict(title='Score'))
 
 
 def get_sample(env, actor, max_itr=1000):
     total_rwd = 0.0
     state = env.reset()
     for i in range(max_itr):
-        a = actor(torch.from_numpy(state).unsqueeze(0).to(device)) 
+        a = actor(torch.from_numpy(state.astype(np.float32)).unsqueeze(0).to(device)) 
         state, rwd, done, _ = env.step(a.max(1)[1].cpu().item())
         if i % 50 == 0:
-            vis.image(utils.preprocess(env.env._get_image()), win=win1)
+            env.render()
         total_rwd += rwd
         if done:
             break
@@ -91,7 +84,7 @@ n_best = int(0.05 * BATCH_SIZE)
 for n in range(n_episodes):
     mean, std, best_rwd = optimize_step(env, actor, mean, std, BATCH_SIZE, n_best)
     print("Episode: %d, Best Reward: %f" % (n, best_rwd))
-    vis.line(X=np.array([n]), Y=np.array([best_rwd]), win=win2, update='append')
+    vis.line(X=np.array([n]), Y=np.array([best_rwd]), win=win, update='append')
 
 print('Complete')
 env.close()
